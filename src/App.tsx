d0fdc0457df7633d9c0b40e644ac7f6b1687ad4e
Howard <howard@HowarddeMacBook-Pro.local>
2026-05-04 13:01:00 +0800

    feat: remove leaflet attribution controls

--- src/App.tsx
+++ src/App.tsx
@@ -194,11 +194,11 @@ export default function App() {
         center={[25.0408, 121.5674]} 
         zoom={14} 
         zoomControl={false}
+        attributionControl={false}
         className="w-full h-full z-[10]"
         style={{ width: '100%', height: '100%' }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://carto.com/">CARTO</a>'
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
         
@@ -261,6 +261,7 @@ export default function App() {
             center={[loc.lat, loc.lng]} 
             zoom={16} 
             zoomControl={false}
+            attributionControl={false}
             dragging={false}
             scrollWheelZoom={false}
             doubleClickZoom={false}
